a06aaa1b8657a6375a439cf74eec3076f7ba9fcb
Ian Ishmael C. Oderon <ianoderon@gmail.com>
2023-08-09 02:37:08 +0800

    Use gap instead of mr

--- components/LandingPage/Features.tsx
+++ components/LandingPage/Features.tsx
@@ -11,18 +11,13 @@ function Features(props: LandingPageFeaturesProps) {
   const { header, body, img, isTextLeft = true } = props;
   return (
     <section className={cx('w-full text-gray-800 py-16 bg-gradient-to-r from-gray-100 to-gray-200')}>
-      <div className="lg:w-lg grid grid-cols-3 grid-row-1 mx-auto">
-        <div
-          className={cx('col-span-2  flex flex-col justify-center', {
-            'mr-10': !isTextLeft,
-            'ml-10': isTextLeft,
-          })}
-        >
+      <div className="lg:w-lg grid grid-cols-5 gap-x-10 grid-row-1 mx-auto">
+        <div className={cx('col-span-3 flex flex-col justify-center')}>
           <div className="text-5xl font-extrabold mb-8">{header}</div>
           <p className="text-xl">{body}</p>
         </div>
         <div
-          className={cx('col-span-1', {
+          className={cx('col-span-2', {
             'order-first': isTextLeft,
           })}
         >
