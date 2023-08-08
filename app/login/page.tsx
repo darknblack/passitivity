05fb643e3f39b5f326cddb25dc4ec5ebe1339ae5
Ian Ishmael C. Oderon <ianoderon@gmail.com>
2023-08-09 02:14:05 +0800

    Added state for form

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,8 +3,15 @@
 // import { useEffect } from 'react';
 import loginBg from '@/assets/login.png';
 import { Label, TextInput, Checkbox, Button } from 'flowbite-react';
+import { useState } from 'react';
 
 export default function Account() {
+  const [state, setState] = useState({
+    email: '',
+    password: '',
+    confirmpassword: '',
+    isTosAccepted: false,
+  });
 
   return (
     <div className="h-screen w-screen flex justify-center items-center bg-gray-100">
@@ -16,34 +23,58 @@ export default function Account() {
             backgroundSize: 'cover',
           }}
         ></div>
-        <div className="p-16 flex-1">
+        <form className="p-16 flex-1">
           <h2 className="text-3xl font-bold mb-6">Create your Free Account</h2>
-          <div className='mb-6'>
+          <div className="mb-6">
             <Label htmlFor="email" value="Email" />
-            <TextInput id="email" type="text" className='mt-2'></TextInput>
+            <TextInput
+              id="email"
+              type="text"
+              className="mt-2"
+              value={state.email}
+              onChange={e => setState({ ...state, email: e.target.value })}
+            ></TextInput>
           </div>
-          <div className='mb-6'>
+          <div className="mb-6">
             <Label htmlFor="password" value="Password" />
-            <TextInput id="password" type="password" className='mt-2'></TextInput>
+            <TextInput
+              id="password"
+              type="password"
+              className="mt-2"
+              value={state.password}
+              onChange={e => setState({ ...state, password: e.target.value })}
+            ></TextInput>
           </div>
-          <div className='mb-6'>
+          <div className="mb-6">
             <Label htmlFor="confirmpassword" value="Confirm Password" />
-            <TextInput id="confirmpassword" type="password" className='mt-2'></TextInput>
+            <TextInput
+              id="confirmpassword"
+              type="password"
+              className="mt-2"
+              value={state.confirmpassword}
+              onChange={e => setState({ ...state, confirmpassword: e.target.value })}
+            ></TextInput>
           </div>
-          <div className='mb-6'>
-            <Checkbox />
+          <div className="mb-6">
+            <Checkbox
+              checked={state.isTosAccepted}
+              onChange={() => {
+                setState({
+                  ...state,
+                  isTosAccepted: !state.isTosAccepted,
+                });
+              }}
+            />
             <span className="ml-1.5 font-[500]">I accept the Terms and Conditions</span>
           </div>
-          <Button className="w-full mb-6">
+          <Button className="w-full mb-6" type="submit">
             Create Account
           </Button>
-          <div className='mb-6 font-[500]'>
+          <div className="mb-6 font-[500]">
             <span className="mr-1">Already have an account?</span>
-            <span className='text-primary-700'>
-              Login here
-            </span>
+            <span className="text-primary-700">Login here</span>
           </div>
-        </div>
+        </form>
       </div>
     </div>
   );
